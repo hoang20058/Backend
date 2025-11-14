b3c170a60ed5c9f554d506aee95733fb77286773
TamNguyen <tn626380@gmail.com>
2025-11-14 15:09:36 +0700

    đã thêm class tắt security login (congig->unsecurityconfig)

---
+++ Backend/src/main/java/com/javaproject/Backend/config/UnSecurityConfig.java
@@ -0,0 +1,20 @@
+// Tắt bắt buộc login (auto login)
+
+package com.javaproject.Backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class UnSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+}

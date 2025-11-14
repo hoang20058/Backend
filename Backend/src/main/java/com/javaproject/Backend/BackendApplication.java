15000f663bc74c95706162cea0ac286f7cd4477e
TamNguyen <tn626380@gmail.com>
2025-11-14 15:49:46 +0700

    huỷ tự tạo database

--- Backend/src/main/java/com/javaproject/Backend/BackendApplication.java
+++ Backend/src/main/java/com/javaproject/Backend/BackendApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class }) // (exclude = {
+																		// DataSourceAutoConfiguration.class }): bỏ auto
+																		// tạo database khi chạy. nếu có data base r thì
+																		// bỏ đoạn code này đi
 public class BackendApplication {
 
 	public static void main(String[] args) {
